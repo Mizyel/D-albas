ae16ad2a9d1ec67bc6ca65e40a0c0af04aa88d45
Michelle Morea <xiomara140516@gmail.com>
2023-05-30 01:37:49 -0500

    primera posible interfaz proyecto

---
+++ static/js/script.js
@@ -0,0 +1,103 @@
+// function InicioSecion(){
+//   window.location.href="SesiónIniciada.html"
+//   }
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+});
+
+// Aqui empieza la funcion de ocultar el contenido de la card
+const itemSeleccionado = document.getElementById("item");
+itemSeleccionado.style.color = "white";
+
+
+function verCategoria(cat){
+    const items = document.getElementsByClassName("item");
+    for(let i=0; i < items.length;i++){
+        items[i].style.display = "none";
+    }
+
+    const itemCat = document.getElementsByClassName(cat);
+    for(let i = 0; i<itemCat.length;i++){
+        itemCat[i].style.display = "block";
+    }
+
+    const links = document.querySelectorAll(".panel nav a");
+    links[0].style.color = "#303030";
+    links[1].style.color = "#303030";
+    links[2].style.color = "#303030";
+    links[3].style.color = "#303040";
+
+    const itemSeleccionado = document.getElementById(cat);
+    itemSeleccionado.style.color = "white";
+}
+// Aqui termina la funcion
